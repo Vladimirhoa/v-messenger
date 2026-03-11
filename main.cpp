08fd1a188daa8c5dc3c8b537f7c09b7dd9b18f4b
Vladimirhoa <rozhdestvo.vi@mail.ru>
2026-03-11 23:00:05 +0300

    multithreading and distribution to all clients

--- main.cpp
+++ main.cpp
@@ -3,6 +3,34 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <thread>
+#include <vector>
+std::vector<int> ClientVector;
+void handleClient(int clientSocket){
+    char buf[1024]; // Сообщение которое пришлёт клиент
+    while(true){
+        memset(buf, 0, 1024);
+        int bytes_received = recv(clientSocket, buf, 1024, 0); // сколько байтов получили от клиента
+        if (bytes_received <= 0) {
+            std::cout << "Client disconnected or error." << std::endl;
+            break; // Выходим из внутреннего цикла к accept
+        }
+        std::cout << "Received: " << bytes_received << " bytes" << std::endl;
+        std::cout << "Client message:" << buf << std::endl;
+        for (int i = 0; i < ClientVector.size(); i++){
+            if (ClientVector[i] != clientSocket){
+                send(ClientVector[i], buf, bytes_received, 0);
+            }
+        }
+
+        //выход при вводе exit
+        if (strncmp(buf, "exit", 4) == 0) {
+            std::cout << "Client requested exit." << std::endl;
+            break;
+        }
+    }
+    close(clientSocket);
+}
 int main(){
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // создали сокет
 
@@ -24,31 +52,15 @@ int main(){
         int clientSocket;
         if ((clientSocket = accept(serverSocket, nullptr, nullptr)) < 0){
             std::cout << "Couldn\'t connect to the client" << std::endl;
-            return 1;
+            continue;
         }
-        else{
-            std::cout << "Client connect!" << std::endl;
-            char buf[1024]; // Сообщение которое пришлёт клиент
-            while(true){
-                memset(buf, 0, 1024);
-                int bytes_received = recv(clientSocket, buf, 1024, 0); // сколько байтов получили от клиента
-                if (bytes_received <= 0) {
-                    std::cout << "Client disconnected or error." << std::endl;
-                    break; // Выходим из внутреннего цикла к accept
-                }
-                std::cout << "Received: " << bytes_received << " bytes" << std::endl;
-                std::cout << "Client message:" << buf << std::endl;
-                //выход при вводе exit
-                if (strncmp(buf, "exit", 4) == 0) {
-                    std::cout << "Client requested exit." << std::endl;
-                    break;
-                }
-
-            }
-            close(clientSocket);
-            std::cout << "Connection closed, waiting for next client..." << std::endl;
-        }
+        std::cout << "New client connected!" << std::endl;
+        ClientVector.push_back(clientSocket);
+        // Запускаем поток.
+        std::thread(handleClient, clientSocket).detach();
     }
+    close(serverSocket);
+    return 0;
 
 
 
